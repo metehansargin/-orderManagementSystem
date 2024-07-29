b55ee2a57baadfcc3199afd5a691484592be3da0
metehansargin <msargin1986@gmail.com>
2024-07-29 20:40:00 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import business.UserController;
 import core.Database;
 import core.Helper;
+import entity.User;
+import view.DashboardUI;
 import view.LoginUI;
 
 import java.sql.Connection;
@@ -10,6 +13,11 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         Helper.setTheme();
-        LoginUI loginUI=new LoginUI();
+       // LoginUI loginUI=new LoginUI();
+        UserController userController = new UserController();
+        User user=userController.findByLogin("metehan@sargın.com","123123");
+        DashboardUI dashboardUI=new DashboardUI(user);
+
+
     }
 }
--- src/business/CustomerController.java
+++ src/business/CustomerController.java
@@ -1,2 +1,53 @@
-package business;public class CustomerController {
+package business;
+
+import core.Helper;
+import dao.CustomerDao;
+import entity.Customer;
+
+import java.util.ArrayList;
+
+public class CustomerController {
+    private final CustomerDao customerDao=new CustomerDao();
+    public ArrayList<Customer> findAll(){
+        return this.customerDao.findAll();
+    }
+    public boolean save(Customer customer){
+        return this.customerDao.save(customer);
+    }
+    public Customer findById(int id){
+    return this.customerDao.getById(id);
+    }
+    public boolean update(Customer customer){
+        if(this.findById(customer.getId())==null){
+            Helper.showMsg(customer.getId()+"Adlı kullanıcı bulanamadı");
+            return false;
+        }
+        return this.customerDao.update(customer);
+    }
+    public boolean delete(int id){
+        if(this.findById(id)==null){
+            Helper.showMsg(id+"Adlı kullanıcı silinmedi");
+            return false;
+        }
+        return this.customerDao.delete(id);
+    }
+    public ArrayList<Customer> filter(String name,Customer.TYPE type){
+        //select * from customertwo where LIKE "%TEST%" and type="person"
+        //select * from customertwo where LIKE "%TEST%"
+        //select * from customertwo where type="person"
+        //select * from customertwo
+        String query="SELECT * FROM CUSTOMERTWO";
+        ArrayList<String> whereList=new ArrayList<>();
+        if(name.length()>0){
+            whereList.add("name LIKE '%"+name+"%'");
+        }
+        if(type!=null){
+            whereList.add("type ='"+type+"'");
+        }
+        if(whereList.size()>0){
+            String whereQuery=String.join(" AND ",whereList);
+            query+=" WHERE "+whereQuery;
+        }
+        return this.customerDao.query(query);
+    }
 }
