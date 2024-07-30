08535260b745ef7b8f9c4d74621832f87666d2bc
metehansargin <msargin1986@gmail.com>
2024-07-30 13:16:20 +0300

    update

--- src/business/ProductController.java
+++ src/business/ProductController.java
@@ -1,2 +1,36 @@
-package business;public class ProductController {
+package business;
+
+import core.Helper;
+import dao.CustomerDao;
+import dao.ProductDao;
+import entity.Customer;
+import entity.Product;
+
+import java.util.ArrayList;
+
+public class ProductController {
+    private final ProductDao productDao=new ProductDao();
+    public ArrayList<Product> findAll(){
+        return this.productDao.findAll();
+    }
+    public boolean save(Product product){
+        return this.productDao.save(product);
+    }
+    public Product getByID(int id){
+        return this.productDao.getById(id);
+    }
+    public boolean update(Product product){
+        if(this.getByID(product.getId())==null){
+            Helper.showMsg(product.getId()+"ID kayıtlı ürün bulanamadı");
+            return false;
+        }
+        return this.productDao.update(product);
+    }
+    public boolean delete(int id){
+        if(this.getByID(id)==null){
+            Helper.showMsg(id+"Adlı kullanıcı silinmedi");
+            return false;
+        }
+        return this.productDao.delete(id);
+    }
 }
