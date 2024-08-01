75aafded2562a1f3bf699da2896b2e41888f15bb
metehansargin <msargin1986@gmail.com>
2024-08-01 11:05:11 +0300

    update

--- src/business/BasketController.java
+++ src/business/BasketController.java
@@ -1,2 +1,12 @@
-package business;public class BasketController {
+package business;
+
+import dao.BasketDao;
+import entity.Basket;
+import entity.Customer;
+
+public class BasketController {
+    private final BasketDao basketDao=new BasketDao();
+    public boolean save(Basket basket){
+        return this.basketDao.save(basket);
+    }
 }
