5a6bfb29e4c67733b121916afd7b2fbaaf385615
metehansargin <msargin1986@gmail.com>
2024-08-05 13:30:17 +0300

    update

--- src/business/CartController.java
+++ src/business/CartController.java
@@ -1,4 +1,18 @@
 package business;
 
+import dao.CartDao;
+import dao.CustomerDao;
+import entity.Cart;
+import entity.Customer;
+
+import java.util.ArrayList;
+
 public class CartController {
+    private final CartDao cartDao=new CartDao();
+    public ArrayList<Cart> findAll(){
+        return this.cartDao.findAll();
+    }
+    public boolean save(Cart cart){
+        return this.cartDao.save(cart);
+    }
 }
